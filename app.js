0120f189cb21990c85053f3565b88f7507827c22
bedris <bedris@yahoo.com>
2026-01-27 09:23:12 -0800

    Add function to load a random bird

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 let revealed = false;
 console.log("Bird flashcards loaded");
+function loadRandomBird() {
+  card.classList.remove("flipped");
+  cardBack.classList.remove("revealed");
+  revealed = false;
+
+  currentBird = birds[Math.floor(Math.random() * birds.length)];
+
+  birdImage.src = currentBird.image;
+  birdName.textContent = currentBird.name;
+  audio.src = currentBird.audio;
+}
